7873d3b49a3bf1953ec93c5b07872db11d84efe1
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-08-07 19:03:05 +0500

    Open Close Button Component

---
+++ src/components/Menu.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+      <Image alt='Open Close Icon' src="/open.png" width={20} height={20}/>
+    </div>
+  )
+}
+
+export default Menu
