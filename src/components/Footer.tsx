47d2fd3d73d28dad4a99f7c97244d485e8c4fa4b
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-08-08 01:10:46 +0500

    footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between'>
+    <div className='h-12 sm:text-base md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between'>
       <Link href='/' className='font-bold text-xl'>Cheezious</Link>
-      <p>&copy;2023-All Rights Reserved</p>
+      <p className='text-right'>&copy;2023-All Rights Reserved</p>
     </div>
   )
 }
