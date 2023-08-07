2527fbbb65f36a9959d59d801823b8a17630bbb0
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-08-08 01:24:56 +0500

    working on feat products slider

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Featured = () => {
   return (
-    <div>
-      Featured
+    <div className='w-screen overflow-x-scroll text-red-500 '>
+      {/* Wrapper */}
+      <div className='w-max flex'>
+        {/* Single Items */}
+        <div className='w-screen h-[60vh] flex flex-col items-center justify-around p-4'>
+          {/* Image Container */}
+          <div className='relative flex-1 w-full'>
+            <Image src='/temporary/p1.png' className='object-contain' alt='' fill />
+          </div>
+          {/* Text Container */}
+          <div className='flex flex-1 flex-col gap-4'>
+            <h1 className='text-xl font-bold uppercase'>Title</h1>
+            <p>Description</p>
+            <span className='text-xl font-bold'>$123</span>
+            <button className='bg-red-500 text-white p-2 rounded-md '>Add to Cart</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
