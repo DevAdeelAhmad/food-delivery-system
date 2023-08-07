0934cf592767e423acf358257fc33cabd7c4ba11
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-08-08 01:06:11 +0500

    slider complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
       </div>
       {/* Right Links */}
       <div className="hidden md:flex gap-4 text-md items-center justify-end flex-1">
-        <div className='md:absolute top-3 right-2 lg:static flex items-center  cursor-pointer bg-orange-300 px-1 rounded-md'>
+        <div className='md:absolute top-3 right-2 gap-1 lg:static flex items-center cursor-pointer bg-orange-300 px-1 rounded-md'>
           <Image src='/phone.png' alt='Phone Icon' width={20} height={20} />
           <span >123456789</span>
         </div>
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,35 +1,45 @@
+"use client"
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const data = [
     {
         id: 1,
-        title : 'Always fresh & Always CRISPY & Always HOT',
-        image : '/slide1.png'
+        title: 'always fresh & always crispy & always hot',
+        image: '/slide1.png'
     },
     {
         id: 2,
-        title : "We deliver your order whenever you're in NY",
-        image : '/slide2.png'
+        title: "we deliver your order whenever you're in NY",
+        image: '/slide2.png'
     },
     {
         id: 3,
-        title : 'The best PIZZA to share with your family & friends',
-        image : '/slide3.png'
+        title: 'the best pizza to share with your family & friends',
+        image: '/slide3.jpg'
     }
 ]
 
 const Slider = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    // useEffect(() => {
+    //     const interval = setInterval(() => setCurrentSlide(prev => (prev === data.length - 1 ? 0 : prev + 1)), 2000);
+    //     return () => clearInterval(interval);
+    // }, [])
+
     return (
-        <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
+        <div className='flex flex-col h-[calc(100vh-2rem)] md:h-[calc(100vh-5rem)] lg:h-[calc(100vh)] lg:flex-row bg-fuchsia-50'>
             {/* Text Container */}
-            <div className='h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold'>
-                <h1 className='text-5xl text-center uppercase p-4 md:text-6xl xl:text-7xl'>Test</h1>
+            <div className='h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold lg:h-full lg:w-1/2'>
+                <h1 className='text-center uppercase p-3 md:p-9 text-2xl md:text-5xl lg:text-5xl xl:text-5xl'>
+                    {data[currentSlide].title}
+                </h1>
                 <button className='bg-red-500 text-white py-4 px-8 delay-75 hover:bg-violet-400'>Order Now</button>
             </div>
             {/* Image Container */}
-            <div className='h-1/2 relative w-full'>
-                <Image className='object-cover' src='/slide1.png' alt='Slider Image' fill />
+            <div className='w-full h-1/2 relative lg:h-full lg:w-1/2'>
+                <Image className='object-cover' src={data[currentSlide].image} alt="" fill />
             </div>
         </div>
     )
