d9f4dce4e52a6c6db2424b58178f95e952eebc70
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-08-07 22:24:49 +0500

    hydartion warning solved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning lang="en">
       <body className={inter.className}>
         <Notification/>
         <Navbar/>
