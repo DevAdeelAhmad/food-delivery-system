3743f7f4dd45f7f98b26811c3fb1c4b154876e70
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-08-07 19:11:04 +0500

    working on navbar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Menu = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div>
       <Image alt='Open Close Icon' src="/open.png" width={20} height={20}/>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,19 @@
+import Link from 'next/link'
 import React from 'react'
+import Menu from './Menu'
 
 const Navbar = () => {
+  
   return (
-    <div>
-      Navbar Component
+    <div className='h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 uppercase'>
+      {/* Logo */}
+      <div className='text-lg '>
+        <Link href='/'>Cheezious</Link>
+      </div>
+      {/* Mobile Menu */}
+      <div>
+        <Menu />
+      </div>
     </div>
   )
 }
