bf5b74941486c58320ac580454c2fb3a52236e36
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-08-08 01:45:10 +0500

    working on featured products slider

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import { featuredProducts } from '@/data'
 
 const Featured = () => {
   return (
@@ -7,19 +8,23 @@ const Featured = () => {
       {/* Wrapper */}
       <div className='w-max flex'>
         {/* Single Items */}
-        <div className='w-screen h-[60vh] flex flex-col items-center justify-around p-4'>
-          {/* Image Container */}
-          <div className='relative flex-1 w-full'>
-            <Image src='/temporary/p1.png' className='object-contain' alt='' fill />
-          </div>
-          {/* Text Container */}
-          <div className='flex flex-1 flex-col gap-4'>
-            <h1 className='text-xl font-bold uppercase'>Title</h1>
-            <p>Description</p>
-            <span className='text-xl font-bold'>$123</span>
-            <button className='bg-red-500 text-white p-2 rounded-md '>Add to Cart</button>
-          </div>
-        </div>
+        {
+          featuredProducts.map(item => (
+            <div key={item.id} className='w-screen h-[60vh] flex flex-col items-center justify-around p-4'>
+              {/* Image Container */}
+              {item.img && (<div className='relative flex-1 w-full'>
+                <Image src={item.img} className='object-contain' alt='Product Image' fill />
+              </div>)}
+              {/* Text Container */}
+              <div className='flex flex-1 flex-col items-center text-center gap-4'>
+                <h1 className='text-xl font-bold uppercase'>{item.title}</h1>
+                <p className='p-4'>{item.desc}</p>
+                <span className='text-xl font-bold'>${item.price}</span>
+                <button className='bg-red-500 text-white p-2 rounded-md '>Add to Cart</button>
+              </div>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
