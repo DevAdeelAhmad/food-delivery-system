215f9d7879d7d600cabb29d01568e92c135a9241
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-08-09 17:24:13 +0500

    prisma installed and docker in progress

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -3,3 +3,6 @@ import { NextResponse } from "next/server";
 export const GET = () => {
   return new NextResponse("Hello world", { status: 200 });
 };
+export const POST = () => {
+  return new NextResponse("Hello world", { status: 200 });
+};
